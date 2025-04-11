cf703b73f16a7afb5e27c400aac55fd6952596c5
Ram Prasad Panigrahy <panigrahyr8@gmail.com>
2025-04-11 08:45:30 +0000

    Initial

---
+++ src/main/java/com/example/springboot_redisom_practice/SpringbootRedisomPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_redisom_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRedisomPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRedisomPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_redisom_practice/SpringbootRedisomPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_redisom_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRedisomPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
